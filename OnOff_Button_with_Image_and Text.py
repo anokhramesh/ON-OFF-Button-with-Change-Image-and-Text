5214bd718daf7985335628ab3188a964637d2f27
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-08-26 11:16:28 +0400

    Update OnOff_Button_with_Image_and Text.py

--- OnOff_Button_with_Image_and Text.py
+++ OnOff_Button_with_Image_and Text.py
@@ -1,14 +1,16 @@
 #ON/OFF Button with Change Image and Text
 from tkinter import *
 root=Tk()
-root.geometry('800x600')
+root.geometry('800x600')# set Diamension of window
 root.title("ON/OFF Buttons")
-root.iconbitmap("python_logo_icon.ico")
-global is_on
-is_on =True
+root.iconbitmap("python_logo_icon.ico")# set icon of the Application 
+global is_on# Create a global Variable name is_on
+is_on =True# At initial Set variabel value to boolean True .
+
 # Create a Label
 my_label=Label(root,text="The Switch is ON",fg="green",font=("Helvetica",32))
 my_label.pack(pady=20)
+
 #Define our Switch function
 def Switch():
     global is_on
@@ -21,8 +23,8 @@ def Switch():
         my_label.config(text="The Switch is ON",fg="green")
         is_on=True
 
-on_image =PhotoImage(file='OnLamp-icon.png')
-off_image =PhotoImage(file='OffLamp-icon.png')
+on_image =PhotoImage(file='OnLamp-icon.png')# set image for on
+off_image =PhotoImage(file='OffLamp-icon.png')# set image for off
 on_button=Button(root,image=on_image,borderwidth=0,width=500,height=130,command=Switch)
 on_button.pack(pady=50)
 
